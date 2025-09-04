4060e767cdda3a316d4140c01c94be7f0cf0e972
Connor Redfern <connor.redfern@optusnet.com.au>
2025-09-04 18:34:59 +0800

    cleanup: removed unneeded imports + comments

--- src/pico/eval/call.c
+++ src/pico/eval/call.c
@@ -1,8 +1,3 @@
-#include <stdio.h>
-#include <string.h> // for memcpy
-
-#include "platform/machine_info.h"
-
 #include "pico/eval/call.h"
 #include "pico/values/types.h"
 #include "pico/stdlib/extra.h"
@@ -77,99 +72,6 @@ void* pico_run_expr(Target target, size_t rsize, Allocator* a, ErrorPoint* point
 
     return out;
 }
-
-/*
-// TODO: current implementation destructively modifies the assembly of the expression. probaly want a better solution in the future
-void* pico_run_expr(Target target, size_t rsize, Allocator* a, ErrorPoint* point) {
-    Assembler* ass = target.target;
-    // result generated; 
-    // generated.type = Ok;
-    void* value = mem_alloc(rsize, a);
-
-    // Generate Code which will: 
-    //  1. Copy the final value (on stack) into the memory allocated into the
-    //     'value' variable (above)
-    //  2. Return to C
-#if ABI == SYSTEM_V_64
-    // memcpy (dest = rdi, src = rsi, size = rdx)
-    // retval = rax
-    if (rsize != 0) {
-        build_binary_op(Mov, reg(RDI, sz_64), imm64((int64_t)value), ass, a, point);
-        build_binary_op(Mov, reg(RSI, sz_64), reg(RSP, sz_64), ass, a, point);
-        build_binary_op(Mov, reg(RDX, sz_64), imm64((int64_t)rsize), ass, a, point);
-
-        build_binary_op(Mov, reg(RCX, sz_64), imm64((int64_t)&memcpy), ass, a, point);
-        build_unary_op(Call, reg(RCX, sz_64), ass, a, point);
-        // pop value from stack
-        build_binary_op(Add, reg(RSP, sz_64), imm32(pi_stack_align(rsize)), ass, a, point);
-    }
-    build_nullary_op(Ret, ass, a, point);
-
-#elif ABI == WIN_64
-    // memcpy (dest = rcx, src = rdx, size = r8)
-    // retval = rax
-    if (rsize != 0) {
-        build_binary_op(Mov, reg(RCX, sz_64), imm64((int64_t)value), ass, a, point);
-        build_binary_op(Mov, reg(RDX, sz_64), reg(RSP, sz_64), ass, a, point);
-        build_binary_op(Mov, reg(R8, sz_64), imm64((int64_t)rsize), ass, a, point);
-        build_binary_op(Sub, reg(RSP, sz_64), imm32(32), ass, a, point);
-
-        build_binary_op(Mov, reg(RAX, sz_64), imm64((int64_t)&memcpy), ass, a, point);
-        build_unary_op(Call, reg(RAX, sz_64), ass, a, point);
-        // pop value from stack
-        build_binary_op(Add, reg(RSP, sz_64), imm32(pi_stack_align(rsize) + 32), ass, a, point);
-    }
-    build_nullary_op(Ret, ass, a, point);
-#else
-#error "Unknown calling convention"
-#endif
-
-    U8Array instructions = get_instructions(ass);
-
-    void* dvars = get_dynamic_memory();
-    void* dynamic_memory_space = mem_alloc(4096, a);
-    void* offset_memory_space = mem_alloc(1024, a);
-
-    Allocator old_temp_alloc = set_std_temp_allocator(*a);
-
-    int64_t out;
-    __asm__ __volatile__(
-        // NOTE: When updating to push more registers, make sure to also update assembly
-        //       in abstraction.c
-        "push %%rbp       \n" // Nonvolatile on System V + Win64
-        "push %%rbx       \n" // Nonvolatile on System V + Win64
-        "push %%rdi       \n" // Nonvolatile on Win 64
-        "push %%rsi       \n" // Nonvolatile on Win 64
-        "push %%r15       \n" // for dynamic vars
-        "push %%r14       \n" // for dynamic memory space
-        "push %%r13       \n" // for control/indexing memory space
-        "push %%r12       \n" // Nonvolatile on System V + Win64
-
-        "mov %2, %%r15    \n"
-        "mov %3, %%r14    \n"
-        "mov %4, %%r13    \n"
-        "mov %%rsp, %%rbp \n"
-        "sub $0x8, %%rbp  \n" // Do this to align RSP & RBP? Possibly to account
-                              // for value on stack?
-        "call *%1         \n"
-
-        "pop %%r12        \n"
-        "pop %%r13        \n"
-        "pop %%r14        \n"
-        "pop %%r15        \n"
-        "pop %%rsi        \n"
-        "pop %%rdi        \n"
-        "pop %%rbx        \n"
-        "pop %%rbp        \n"
-        : "=r"(out)
-
-        : "r"(instructions.data), "r"(dvars), "r"(dynamic_memory_space), "r"(offset_memory_space));
-
-    set_std_temp_allocator(old_temp_alloc);
-    mem_free(dynamic_memory_space, a);
-    return value;
-}
-*/
 
 Document* pretty_res(EvalResult res, Allocator* a) {
     Document* out = NULL;
