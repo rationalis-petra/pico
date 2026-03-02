a6fc02650511b3a7d35f2368024e038fb8d7c3b2
Connor Redfern <connor.redfern@optusnet.com.au>
2026-03-02 10:15:15 +0800

    Version Bump

--- src/main.c
+++ src/main.c
@@ -32,7 +32,7 @@
 #include "app/module_load.h"
 #include "app/help_string.h"
 
-static const char* version = "0.1.5";
+static const char* version = "0.1.6";
 
 typedef struct {
     bool debug_print;
